8ba673e978a3f5a6eb91047d6211b49c09007f0e
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-03-02 16:14:45 -0300

    add keypad fix

--- nin-vi-mode.plugin.zsh
+++ nin-vi-mode.plugin.zsh
@@ -69,6 +69,27 @@ bindkey "${terminfo[kent]}" accept-line
 # rebinding <ESC> in normal mode to something harmless solves the problem.
 bindkey -M vicmd '\e' what-cursor-position
 
+# Keypad fixes
+# 0 .
+bindkey -s "^[Op" "0"
+bindkey -s "^[On" "."
+# 1 2 3
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+# 4 5 6
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+# 7 8 9
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+# + -  * /
+bindkey -s "^[Ol" "+"
+bindkey -s "^[OS" "-"
+bindkey -s "^[OR" "*"
+bindkey -s "^[OQ" "/"
 ########### vi-like copy and paste on OSx ##########
 if [ `uname` = "Darwin" ] && (($+commands[pbcopy])); then
   function cutbuffer() {
