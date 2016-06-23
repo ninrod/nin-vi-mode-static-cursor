45818284bd0fa1d2b8f57879e5c4a8082b1277d6
ninrod <filipe.silva@gmail.com>
2016-06-23 00:32:42 -0300

    remove vi-yank hacks

--- nin-vi-mode.plugin.zsh
+++ nin-vi-mode.plugin.zsh
@@ -142,84 +142,31 @@ bindkey -M visual 'u' vi-lowercase
 bindkey -M visual 'U' vi-uppercase
 
 # }}}
-# escape code fixes {{{
+# various escape code fixes {{{
 
-# home key
-bindkey "^[[1~" beginning-of-line
+# home, end, delete and backspace
+bindkey "^[[1~" beginning-of-line # home key
+bindkey "^[[4~" end-of-line # end key
+bindkey "^[[3~" delete-char # delete key
+bindkey "^H" backward-delete-char # backspace key
+bindkey "^?" backward-delete-char # backspace key
 
-# end key
-bindkey "^[[4~" end-of-line
-
-# delete key
-bindkey "^[[3~" delete-char
-
-# backspace key
-bindkey "^H" backward-delete-char
-bindkey "^?" backward-delete-char
-
-# numeric keypad return (enter)
-bindkey "${terminfo[kent]}" accept-line
-# Keypad fixes
-# 0 .
+# Numeric Keypad fixes
+bindkey "${terminfo[kent]}" accept-line # numeric keypad return (enter)
 bindkey -s "^[Op" "0"
 bindkey -s "^[On" "."
-# 1 2 3
 bindkey -s "^[Oq" "1"
 bindkey -s "^[Or" "2"
 bindkey -s "^[Os" "3"
-# 4 5 6
 bindkey -s "^[Ot" "4"
 bindkey -s "^[Ou" "5"
 bindkey -s "^[Ov" "6"
-# 7 8 9
 bindkey -s "^[Ow" "7"
 bindkey -s "^[Ox" "8"
 bindkey -s "^[Oy" "9"
-# + -  * /
 bindkey -s "^[Ol" "+"
 bindkey -s "^[OS" "-"
 bindkey -s "^[OR" "*"
 bindkey -s "^[OQ" "/"
-
-# }}}
-# vi-like copy and paste on OSx {{{
-
-if [ `uname` = "Darwin" ] && (($+commands[pbcopy])); then
-  function cutbuffer() {
-    zle .$WIDGET
-    echo $CUTBUFFER | pbcopy
-  }
-
-  zle_cut_widgets=(
-    vi-backward-delete-char
-    vi-change
-    vi-change-eol
-    vi-change-whole-line
-    vi-delete
-    vi-delete-char
-    vi-kill-eol
-    vi-substitute
-    vi-yank
-    vi-yank-eol
-  )
-  for widget in $zle_cut_widgets
-  do
-    zle -N $widget cutbuffer
-  done
-
-  function putbuffer() {
-    zle copy-region-as-kill "$(pbpaste)"
-    zle .$WIDGET
-  }
-
-  zle_put_widgets=(
-    vi-put-after
-    vi-put-before
-  )
-  for widget in $zle_put_widgets
-  do
-    zle -N $widget putbuffer
-  done
-fi
 
 # }}}
