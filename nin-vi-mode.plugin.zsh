fc9af7308c607555db8af4f6319b1ad5ea9ec071
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-06-30 18:10:09 -0300

    add <C-c> sinalization

--- nin-vi-mode.plugin.zsh
+++ nin-vi-mode.plugin.zsh
@@ -1,3 +1,17 @@
+# terminal colors {{{
+
+TC='\e['
+Rst="${TC}0m"     # Reset all coloring and style
+Black="${TC}30m";
+Red="${TC}31m";
+Green="${TC}32m";
+Yellow="${TC}33m";
+Blue="${TC}34m";
+Purple="${TC}35m";
+Cyan="${TC}36m";
+White="${TC}37m";
+
+# }}}
 # helper functions {{{
 
 # nin-cursor-shape: Change the cursor shape under iTerm2
@@ -116,6 +130,7 @@ bindkey "^M" nin-accept-line
 # when we cancel the current command, return the cursor shape to block
 TRAPINT() {
   nin-cursor-shape-block
+  print -n "${Purple}[${Cyan}ctrl-c${Purple}]${Rst}"
   return $(( 128 + $1 ))
 }
 
